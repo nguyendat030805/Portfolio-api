eea26de60f76b05e7bfc0faa1a3ea31f9910ca53
Nguyen Van Dat <dat656473@gmail.com>
2026-04-07 17:02:02 +0700

    fix: add home route to verify deployment

--- routes/web.php
+++ routes/web.php
@@ -2,7 +2,9 @@
 
 use App\features\auths\logins\controllers\LoginController;
 use Illuminate\Support\Facades\Route;
-
+Route::get('/', function () {
+    return response()->json(['status' => 'success', 'message' => 'Backend is Live!']);
+});
 Route::prefix('auth')->group(function () {
     Route::get('/google', [LoginController::class, 'rejectGoogleLogin']);
     Route::get('/google/callback', [LoginController::class, 'handleGoogleLogin']);
